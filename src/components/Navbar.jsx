2d6ed34860cd64c9b14aca785085a5b380795ecb
admd4326 <admd4326@gmail.com>
2025-09-13 23:48:28 +0300

    complete the navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Button } from "./Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export const Navbar = () => {
   return (
     <nav className="top-nav">
+      <FontAwesomeIcon icon={faBars} size="xl" className="faBars" />
       <p className="title">Community App</p>
       <Button text="Create Account" type="outline" />
     </nav>
